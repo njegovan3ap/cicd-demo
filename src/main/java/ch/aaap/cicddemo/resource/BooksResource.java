92130d7a05eae73cc1794645e1b8f6f22a27813b
Njegovan Obradovic <njegovan@3ap.ch>
2019-06-18 16:44:54 +0200

    circleci - cloudfoundry - final test

---
+++ src/main/java/ch/aaap/cicddemo/resource/BooksResource.java
@@ -0,0 +1,21 @@
+package ch.aaap.cicddemo.resource;
+
+import ch.aaap.cicddemo.model.Book;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("books")
+public class BooksResource {
+
+    @GetMapping
+    public List<Book> books() {
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("title1", "author1"));
+        return books;
+    }
+}
